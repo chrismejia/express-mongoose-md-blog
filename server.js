bc2b49f1b4bff2aa0e534b250c6be2509c60afa0
Christian Mejia <chrismejia10@gmail.com>
2022-02-20 06:26:03 -0500

    feat: give express ability to read encoded form data

--- server.js
+++ server.js
@@ -5,22 +5,35 @@ const talents = require("./__test__/test_talents");
 
 const app = express();
 
+/**
+ * Express settings
+ */
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+/**
+ * Index Routing
+ */
+app.get("/", (req, res) => {
+  res.render("talents/index", { talents });
+});
+
+/**
+ * Router Mounts
+ */
+app.use("/talents", talentsRouter);
+
+/**
+ * Connections
+ */
+app.listen(3001, () => {
+  console.log("Holo talent server listening on port 3001");
+});
+
 mongoose.connect("mongodb://localhost/holoproTalents", () => {
   console.log("Connected to holoproTalents DB!"),
     (err) => {
       console.error("holoproTalents DB error:");
       console.error(err);
     };
-});
-
-app.set("view engine", "ejs");
-
-app.use("/talents", talentsRouter);
-
-app.get("/", (req, res) => {
-  res.render("talents/index", { talents });
-});
-
-app.listen(3001, () => {
-  console.log("Holo talent server listening on port 3001");
 });
