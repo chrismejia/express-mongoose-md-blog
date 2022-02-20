c2ee851bb1284b5e7ea1fe195dc37211c7444796
Christian Mejia <chrismejia10@gmail.com>
2022-02-20 06:24:25 -0500

    refactor: remove `unitMembers` from required model

--- models/Talent.js
+++ models/Talent.js
@@ -4,8 +4,8 @@ const talentSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     debutDate: { type: Date, required: true },
+    tagline: { type: String, required: true },
     unitName: { type: String, required: true, enum: ["5th Gen - NePoLaBo"] },
-    unitMembers: { type: [mongoose.SchemaTypes.ObjectId], ref: "Talent" },
     youtube: { type: String, required: true },
     twitter: { type: String, required: true },
     bioBlurb: { type: String, required: true },
